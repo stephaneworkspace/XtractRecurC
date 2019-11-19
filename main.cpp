9df3620419e370d4f38af72fddc421fc2766b1a0
stephaneworkspace <s.bressani@bluewin.ch>
2019-11-19 14:51:51 +0100

    backup

--- main.cpp
+++ main.cpp
@@ -133,13 +133,37 @@ int main(int argc, char *argv[])
     for (auto &a : vFiles) {
         switch (a.TypeExt()) {
             case eCompress::TAR_GZ:
+                command = "mkdir " + a.file_path + a.ExtractionNewPath();
+                system(command.c_str());
+                command = "tar xvf " + a.CompletePathFile() + " -C " +
+                          a.ExtractionNewPath();
+                system(command.c_str());
+                command = "rm " + a.CompletePathFile();
+                system(command.c_str());
+                break;
             case eCompress::GZ:
+                command = "mkdir " + a.file_path + a.ExtractionNewPath();
+                system(command.c_str());
+                command = "gunzip " + a.CompletePathFile() + " > " +
+                          a.ExtractionNewPath();
+                system(command.c_str());
+                command = "rm " + a.CompletePathFile();
+                system(command.c_str());
+                break;
             case eCompress::TGZ:
+                command = "mkdir " + a.file_path + a.ExtractionNewPath();
+                system(command.c_str());
+                command = "tar zxvf " + a.CompletePathFile() + " -C " +
+                          a.ExtractionNewPath();
+                system(command.c_str());
+                command = "rm " + a.CompletePathFile();
+                system(command.c_str());
+                break;
             case eCompress::ZIP:
                 command = "mkdir " + a.file_path + a.ExtractionNewPath();
                 system(command.c_str());
-                command = "tar xvf " + a.CompletePathFile() +
-                          " -C /home/stephane/Temp/Test/";
+                command = "tar xvf " + a.CompletePathFile() + " -C " +
+                          a.ExtractionNewPath();
                 system(command.c_str());
                 command = "rm " + a.CompletePathFile();
                 system(command.c_str());
